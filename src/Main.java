fa5a1e549242136ff9d22bc50958500e31ade748
cherryshneva <cherryshneva@gmail.com>
2023-11-27 21:17:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15100;
+        int rublesPerMile = 20;
+
+        int bonus = (price / rublesPerMile);
+
+        System.out.println(bonus);
+
+    }
+}
